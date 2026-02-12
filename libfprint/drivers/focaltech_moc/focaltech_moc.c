2c7842c905147a2d127c1b168b2e9d432b8c91a4
Daniel Schaefer <dhs@frame.work>
2026-02-12 16:08:31 +0800

    focaltech_moc: add new pid:0x077A pid:0x079A
    Tested on working hardware with examples/enroll and examples/verify
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- libfprint/drivers/focaltech_moc/focaltech_moc.c
+++ libfprint/drivers/focaltech_moc/focaltech_moc.c
@@ -35,6 +35,8 @@ static const FpIdEntry id_table[] = {
   { .vid = 0x2808,  .pid = 0xa57a,  },
   { .vid = 0x2808,  .pid = 0xa78a,  },
   { .vid = 0x2808,  .pid = 0x1579,  },
+  { .vid = 0x2808,  .pid = 0x077A,  },
+  { .vid = 0x2808,  .pid = 0x079A,  },
   { .vid = 0,  .pid = 0,  .driver_data = 0 },   /* terminating entry */
 };
 
